cdb50947eb55c6deebe40c323ff664bfb1c7c70d
Nick Weiland <njweiland23@gmail.com>
2023-11-01 15:23:13 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(record) {
+    record = record.find(obj => obj.result === "W")
+    if (record) {
+        return record.year
+    } else {
+        return undefined
+    }
+}
